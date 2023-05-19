4043fb9a3b9dc1c9e57b1be9ed80080092798dfe
TheGiantNinja <yasserfedsi34@gmail.com>
2023-05-19 13:28:22 +0200

    Done

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var inviteButton = document.querySelector(".invite-button");
+    
+    inviteButton.addEventListener("click", function() {
+      // Perform any actions you need when the button is clicked
+      // For example, you can show a message or perform an API request
+      console.log("Button clicked!");
+    });
+  });
+  
